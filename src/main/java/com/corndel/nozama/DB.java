b982b7363939f81fdce90178052a557c3d0db883
mik-rzo <michael.panong3@gmail.com>
2024-10-14 11:23:10 +0100

    completed exercise 2

--- src/main/java/com/corndel/nozama/DB.java
+++ src/main/java/com/corndel/nozama/DB.java
@@ -1,14 +1,13 @@
 package com.corndel.nozama;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-  /** TODO: Set the dbUrl */
-  public static final String dbUrl = null;
+  public static final String dbUrl = "jdbc:sqlite:nozama.db";
 
   public static Connection getConnection() throws SQLException {
-    /** TODO: finish this method */
-    return null;
+    return DriverManager.getConnection(dbUrl);
   }
 }
